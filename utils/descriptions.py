9c96976eec72b2da861ee37a4e4281e5621607f3
maderiu <marco.deriu@soccerment.com>
2024-03-26 22:53:45 +0100

    code completed

---
+++ utils/descriptions.py
@@ -0,0 +1,10 @@
+
+class Descriptions:
+    stats = '''
+    xG: Expected goals: Estimates the number of goals a player or team should have scored.
+    xA: Expected assists: Estimates the number of assists a player should have recorded, given the quality of the passes or chances they created.
+    xT: Expected threat: Measures the increase of team scoring probability due to actions that moves the ball in more threatening zones from carries or passes,
+    xT_from_passes: expected threat from passes. Measures the increase of team scoring probability due to actions from passes that moves the ball in more threatening zones,
+    xT_from_carries: xT from carries. Measures the increase of team scoring probability due to actions from carries that moves the ball in more threatening zones,
+    offensive AERO: Elo-based rating converted into the probability (0-100) of winning an offensive aerial duel against an average player, adjusting after each encounter based on the outcome and the defensive rating of the opponent faced, reflecting a player effectiveness in offensive aerial duels',
+    '''
